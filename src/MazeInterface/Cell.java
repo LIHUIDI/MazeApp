dfd67b90ab3e8bc13560b23144e66543532de791
LIHUIDI <lhdistimes@gmail.com>
2016-04-24 21:27:52 +1000

    initial project version

---
+++ src/MazeInterface/Cell.java
@@ -0,0 +1,9 @@
+package MazeInterface;
+
+import java.awt.Graphics;
+
+public abstract class Cell {
+
+	public abstract void paintCell(Graphics g);
+
+}
---
+++ src/MazeInterface/Maze.java
@@ -0,0 +1,13 @@
+package MazeInterface;
+
+import java.awt.Component;
+import java.awt.Graphics;
+
+public abstract class Maze extends Component{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public abstract void generateMaze(Graphics g);
+}
