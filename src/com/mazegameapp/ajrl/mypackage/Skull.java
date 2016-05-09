794dcaecb2dae13ec7e2b41996c83439a04eee56
LIHUIDI <lhdistimes@gmail.com>
2016-05-09 22:14:24 +1000

    add another item skull, when player encouter skull in cell, score would be decreased

---
+++ src/com/mazegameapp/ajrl/mypackage/Skull.java
@@ -0,0 +1,40 @@
+package com.mazegameapp.ajrl.mypackage;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Skull implements Item{
+int value;
+	
+	private BufferedImage image;
+
+	private void loadImage() {
+	     try {                
+	        image = ImageIO.read(new File("resources/skull.png"));
+	     } catch (IOException ex) {
+	            // handle exception...
+	     }
+	}
+	
+	@Override
+	public int getValue() {
+		return value;
+	}
+	
+	@Override
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+
+	@Override
+	public void drawItem(Graphics g, SquareCell squareCell) {
+		loadImage();
+		g.drawImage(image, squareCell.getX()+2, squareCell.getY()+2, squareCell.getWidth()-4, squareCell.getWidth()-4, null, null);
+	}
+
+}
